78995418ea32af4998b4ef23a309ef781a8ca9f8
sreiss <stephane.reiss@gmail.com>
2018-09-23 14:24:15 -0400

    Restarted from scratch

--- mtg/mtg.ino
+++ mtg/mtg.ino
@@ -1,65 +1,161 @@
-#include <Wire.h>
-#include <BreezyArduCAM.h>
-#include <SPI.h>
 #include <Servo.h>
 
-Servo myservo;
+Servo cardServo;
 
-static const int CS = 7;
-static const int CARD_HOLDER_SERVO = 5;
+// MOTORS
+static const int MOTOR_CARD = 5;
+static const int MOTOR_CARD_TIME = 250; //ms
+static const int SERVO_CARD = 2;
+static const int SERVO_CLOSE = 60;
+static const int SERVO_OPEN = 130;
+static const int SERVO_OPEN_TIME = 300; //ms
+
+// BUTTONS
+static const int BUTTON_A = 7;
+static const int BUTTON_B = 8;
+
+// SENSORS
 static const int CARD_HOLDER_SENSOR = 6;
-static const int RED = 2;
 
-// the following variables are unsigned longs because the time, measured in
-// milliseconds, will quickly become a bigger number than can be stored in an int.
-unsigned long last_debounce_time = 0;  // the last time the output pin was toggled
-unsigned long debounce_delay = 50;    // the debounce time; increase if the output flickers
+// LEDS
+static const int LED_RED = 4;
+static const int LED_GREEN = 3;
 
-int sensor_value;
-int previous_sensor_value = HIGH;
+// CARD COLORS
+static const int CARD_NONE = 0;
+static const int CARD_UNKNOWN = -1;
+static const int CARD_RED = 1;
+static const int CARD_GREEN = 2;
+static const int CARD_BLUE = 3;
+static const int CARD_BLACK = 4;
+static const int CARD_WHITE = 5;
+static const int CARD_COLORLESS = 6;
+static const int CARD_OTHER = 7;
 
-Serial_ArduCAM_FrameGrabber fg;
-
-ArduCAM_Mini_2MP myCam(CS, &fg);
 
 void setup(void)
 {
-  // ArduCAM Mini uses both I^2C and SPI buses
-  Wire.begin();
-  SPI.begin();
-
-  // Fastest baud rate 921600 (change to 115200 for Due)
-  Serial.begin(921600);
-
-  // Begin capturing in JPEG
-  myCam.beginJpeg640x480();
-
-  myservo.attach(CARD_HOLDER_SERVO);
-  myservo.write(90);
-
+  pinMode(MOTOR_CARD, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(SERVO_CARD, OUTPUT);
+  pinMode(BUTTON_A, INPUT_PULLUP);
+  pinMode(BUTTON_B, INPUT_PULLUP);
   pinMode(CARD_HOLDER_SENSOR, INPUT);
-  pinMode(RED, OUTPUT);
+
+
+  cardServo.attach(SERVO_CARD);
+  cardServo.write(SERVO_CLOSE);
 }
 
-void loop(void)
+bool serving_new_card = false;
+unsigned long serving_card_start = 0;
+void sendCardtoAnalyse(void)
+{
+  int button_a = digitalRead(BUTTON_A);
+  if (!serving_new_card && button_a == LOW) {
+    serving_new_card = true;
+    serving_card_start = millis();
+    analogWrite(MOTOR_CARD, 100);
+  }
+
+  if (serving_new_card && (millis() - serving_card_start) > MOTOR_CARD_TIME) {
+    serving_new_card = false;
+    analogWrite(MOTOR_CARD, 0);
+  }
+}
+
+int card_detected = HIGH;
+int previous_card_reading_value = HIGH;
+unsigned int last_debounce_time_card = 0;
+int debounce_delay = 50; //ms
+bool isCardReadyToAnalyze(void)
 {
   int card_reading = digitalRead(CARD_HOLDER_SENSOR);
 
-  if (card_reading != previous_sensor_value) {
+  if (card_reading != previous_card_reading_value) {
     // reset the debouncing timer
-    last_debounce_time = millis();
+    last_debounce_time_card = millis();
   }
 
-  if ((millis() - last_debounce_time) > debounce_delay) {
-    if (card_reading != sensor_value) {
-      sensor_value = card_reading;
-
-      if (sensor_value == LOW) {
-        Serial.write(42);
-      }
+  if ((millis() - last_debounce_time_card) > debounce_delay) {
+    if (card_reading != card_detected) {
+      card_detected = card_reading;
     }
   }
-  previous_sensor_value = card_reading;
-  myCam.capture();
+  previous_card_reading_value = card_reading;
 
+  return card_detected == LOW;
+}
+
+bool sorting_card = false;
+unsigned long sorting_card_start = 0;
+void sendCardToSorting(bool send)
+{
+  if (!sorting_card && send) {
+    sorting_card = true;
+    sorting_card_start = millis();
+    cardServo.write(SERVO_OPEN);
+  }
+
+  if (sorting_card && !send && (millis() - sorting_card_start) > SERVO_OPEN_TIME) {
+    sorting_card = false;
+    cardServo.write(SERVO_CLOSE);
+  }
+}
+
+bool analyzing_card = false;
+bool tmp = false;
+unsigned long analyzing_card_start = 0;
+int card_color = CARD_NONE;
+int getCardColor()
+{
+  if (!tmp) {
+    return CARD_NONE;
+  }
+
+  if (!analyzing_card) {
+    digitalWrite(LED_GREEN, HIGH);
+    analyzing_card = true;
+    analyzing_card_start = millis();
+  }
+
+  if (analyzing_card && (millis() - analyzing_card_start) > 2000) {
+    digitalWrite(LED_GREEN, LOW);
+    analyzing_card = false;
+    tmp = false;
+    return CARD_RED;
+  }
+
+  return CARD_NONE;
+}
+
+
+void loop(void)
+{
+  sendCardtoAnalyse();
+  card_color = getCardColor();
+  if (isCardReadyToAnalyze()) {
+    digitalWrite(LED_RED, HIGH);
+    tmp = true;
+    if (card_color) {
+      sendCardToSorting(true);
+    }
+  } else {
+    digitalWrite(LED_RED, LOW);
+    sendCardToSorting(false);
+    //    card_color = CARD_NONE;
+  }
+  debugSendCardToSorting();
+}
+
+
+//-------------------------------------
+// DEBUG FUNCTIONS
+//-------------------------------------
+void debugSendCardToSorting(void)
+{
+  if (digitalRead(BUTTON_B) == LOW) {
+    sendCardToSorting(true);
+  }
 }
