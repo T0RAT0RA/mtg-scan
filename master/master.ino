d37506b5fbca01755f38b5775e8124e7f6a0f8fd
sreiss <stephane.reiss@gmail.com>
2018-09-29 23:24:58 -0400

    Added scrython api and refactor card reader.
    Added debug buttons to master.

--- master/master.ino
+++ master/master.ino
@@ -2,7 +2,15 @@
 #include <BreezyArduCAM.h>
 #include <SPI.h>
 
+// CAMERA
 const int CS = 7;
+
+// BUTTONS
+const int BUTTON_C = 6;
+const int BUTTON_D = 5;
+
+// LEDS
+const int LED_GREEN = 4;
 
 Serial_ArduCAM_FrameGrabber fg;
 ArduCAM_Mini_2MP cardCam(CS, &fg);
@@ -14,6 +22,9 @@ void setup(void)
 
   Serial.begin(921600);
   cardCam.beginJpeg800x600();
+  pinMode(BUTTON_C, INPUT_PULLUP);
+  pinMode(BUTTON_D, INPUT_PULLUP);
+  pinMode(LED_GREEN, OUTPUT);
 }
 
 enum states {
@@ -26,41 +37,70 @@ states state = Motor1;
 
 void sendToSlave(byte value)
 {
-  Wire.beginTransmission(8); 
+  Wire.beginTransmission(8);
   Wire.write(value);
   Wire.endTransmission();
 }
 unsigned long startcaptureTime;
- 
+
+
+bool modeAuto = true;
 void loop(void)
 {
-   switch (state) {
-        case Motor1:
+  debugCamera();
+  
+  if (modeAuto) {
+    switch (state) {
+      case Motor1:
         {
+          Serial.println("Motor1");
           sendToSlave(1);
-          delay(1500);
-          
+
           state = Capture;
           startcaptureTime = millis();
           cardCam.singleCapture(true);
         }
         break;
-        case Capture:
+      case Capture:
         {
+          Serial.println("Capture");
           cardCam.singleCapture(false);
 
-          if ( millis() - startcaptureTime > 1000)
+          if ( millis() - startcaptureTime > 5000)
           {
-              state = Motor2;
+            state = Motor2;
           }
         }
         break;
-        case Motor2:
+      case Motor2:
         {
+          Serial.println("Motor2");
           sendToSlave(2);
-          delay(500);
           state = Motor1;
         }
         break;
-   }
+    }
+  }
+}
+
+bool capture = false;
+bool capturing = false;
+void debugCamera(void) {
+  if (!capturing && !capture && digitalRead(BUTTON_C) == LOW) {
+    capture = true;
+    capturing = true;
+    startcaptureTime = millis();
+    cardCam.singleCapture(true);
+  }
+
+  if (capturing) {
+    cardCam.singleCapture(false);
+    if (millis() - startcaptureTime > 1000) {
+      capture = false;
+      capturing = false;
+    }
+  }
+
+
+
 }
--- mtg-card-reader.py
+++ mtg-card-reader.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
-import sys, base64, glob, time
-import random
-import boto3
-import warnings
+import sys, base64, os, glob, time
+import itertools
+import boto3, scrython
 import serial
 import serial.tools.list_ports
 from sys import stdout
 
 
 BAUD = 921600
-OUTFILENAME = './captures/card.jpg'
+CAPTURE_FOLDER = './captures/'
+CARD_NAME = 'card*.jpg'
 
 
 def analyze_card(path):
@@ -28,24 +28,29 @@ def analyze_card(path):
     if response['TextDetections']:
         lines = [line for line in response['TextDetections'] if line['Type'] == 'LINE']
         if lines:
+            name = lines[0]['DetectedText']
+            colors = []
+            error = None
+            # edition = lines[-1]['DetectedText']
+
+            try:
+                card = scrython.cards.Named(exact=name)
+                colors = card.color_identity()
+            except Exception as e:
+                error = str(e)
+
             return {
-                "img": path,
-                "name": lines[0]['DetectedText'],
-                "edition": lines[-1]['DetectedText'],
+                'name': name,
+                'colors': colors,
+                'error': error
             }
         else:
             raise Exception('No lines found.')
     else:
         raise Exception('No text found.')
 
-
-def capture_card():
-    print('\nStart capturing')
-    # Send "start capture" message
-    sendbyte(port, 1)
-
-    # Open output file
-    outfile = open(OUTFILENAME, 'wb')
+def capture_card(path):
+    print('\nWaiting capture...')
 
     # Loop over bytes from Arduino for a single image
     written = False
@@ -61,6 +66,8 @@ def capture_card():
 
             # Start-of-image sentinel bytes: write previous byte to temp file
             if ord(currbyte) == 0xd8 and ord(prevbyte) == 0xff:
+                # Open output file
+                outfile = open(path, 'wb')
                 outfile.write(prevbyte)
                 written = True
 
@@ -76,29 +83,14 @@ def capture_card():
         # Track previous byte
         prevbyte = currbyte
 
-    # Send "stop" message
-    sendbyte(port, 0)
-
-    print('\nDone capturing')
+    return True;
 
 # helpers  --------------------------------------------------------------------------
-def num2bytes(n):
-    return [n&0xFF, (n>>8)&0xFF, (n>>16)&0xFF, (n>>24)&0XFF]
-
-def dump(msg):
-    stdout.write(msg)
-    stdout.flush()
-
 def getack(port):
     stdout.write(port.readline().decode())
 
 def sendbyte(port, value):
     port.write(bytearray([value]))
-
-def ackcheck(port, msg):
-    line = port.readline().decode()
-    assert(msg in line)
-
 
 
 CARD_NONE = 0;
@@ -112,7 +104,11 @@ CARD_COLORLESS = 6;
 CARD_OTHER = 7;
 
 # main  --------------------------------------------------------------------------
-if __name__ == '__main__':
+cards = {
+  'success': [],
+  'error': []
+}
+def main():
     # Find Arduino port
     arduino_ports = [
         p.device
@@ -120,34 +116,71 @@ if __name__ == '__main__':
         if p.manufacturer is not None and 'Arduino' in p.manufacturer
     ]
 
-    if not arduino_ports:
-        raise IOError("No Arduino found")
-    if len(arduino_ports) > 1:
-        warnings.warn('Multiple Arduinos found - using the first')
+    # if not arduino_ports:
+    #     raise IOError("No Arduino found")
+    # if len(arduino_ports) > 1:
+    #     print('Multiple Arduinos found - using the first')
 
     # Open connection to Arduino with a timeout of two seconds
-    port = serial.Serial(arduino_ports[0], BAUD, timeout=5)
+    # port = serial.Serial(arduino_ports[0], BAUD, timeout=None)
 
     # Report acknowledgment from camera
-    getack(port)
+    # getack(port)
     time.sleep(0.2)
 
+    print('Clearing capture folder: %s' % CAPTURE_FOLDER)
+    for filename in glob.glob(CAPTURE_FOLDER + CARD_NAME):
+        # os.remove(filename)
+
     print('Ready, waiting incoming data...')
+
+    index = 1;
     while(1):
-        response = port.read(1)
-        if len(response) == 1 and ord(response) == 42:
-            print('CARD DETECTED.')
-            # time.sleep(1);
-            color = random.choice([
-                CARD_RED,
-                CARD_GREEN,
-                CARD_BLUE,
-                CARD_BLACK,
-                CARD_WHITE,
-            ])
-            print('CARD ANALYSED: %s' % color)
-            sendbyte(port, 43)
-            sendbyte(port, color)
+        path = CAPTURE_FOLDER + CARD_NAME.replace('*', str(index));
+
+        capture_card(path)
+
+        try:
+            card_meta = analyze_card(path)
+        except FileNotFoundError as e:
+            print(str(e))
+            time.sleep(1)
+            continue
+
+        if (card_meta['error']):
+            cards['error'].append(card_meta)
         else:
-            print(response)
-        time.sleep(0.1)
+            cards['success'].append(card_meta)
+        # if len(response) == 1 and ord(response) == 42:
+        #     print('CARD DETECTED.')
+        #     # time.sleep(1);
+        #     color = random.choice([
+        #         CARD_RED,
+        #         CARD_GREEN,
+        #         CARD_BLUE,
+        #         CARD_BLACK,
+        #         CARD_WHITE,
+        #     ])
+        #     print('CARD ANALYSED: %s' % color)
+        #     sendbyte(port, 43)
+        #     sendbyte(port, color)
+        # else:
+        #     print(response)
+        index = index + 1
+
+def printStatus():
+    print('---------')
+    print('Card scanned: %i' % len(cards['success']))
+    if len(cards['error']):
+        print('Card with errors: %i' % len(cards['error']))
+
+    print('Card list:')
+    for card, group in itertools.groupby(cards['success'], key=lambda x:x['name']):
+        print('%s x%i' % (card, len(list(group))))
+    print('---------')
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        printStatus()
